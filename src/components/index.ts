6792766d355fb4256afe26f3e6ab913ef1b2cc32
Anton Vlasov <antonvlasov@MacBook-Pro-MacBook.local>
2024-03-02 18:07:31 +0500

    sprint_1 login page added

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { Dialog } from './dialog';
+export { Input } from './input';
+export { Button } from './button';
+export { Link } from './link';
+export { DialogTitle } from './dialog-title';
+
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import Handlebars from 'handlebars';
+
+import './index.scss';
 import * as Components from './components';
-import * as Pages from './pages';
+import pages from './pages';
+
+Object.entries(Components).forEach(([ name, component ]) => {
+  Handlebars.registerPartial(name, component)
+})
+
+function navigate(page: string) {
+  const [ source, args ] = pages[page];
+  const handlebarsFunct = Handlebars.compile(source);
+  document.body.innerHTML = handlebarsFunct(args);
+}
+
+document.addEventListener('DOMContentLoaded', () => navigate('login'));
