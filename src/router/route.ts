94740661663eef8ce7a4d776a177ecf4cb6d33d4
Anton Vlasov <antonvlasov@MacBook-Pro-MacBook.local>
2024-04-05 17:46:42 +0500

    added router

---
+++ src/router/route.ts
@@ -0,0 +1,43 @@
+import Block from '../tools/block';
+import { navigate } from '../tools/helpers';
+
+export default class Route {
+  private _pathname: string;
+  private _blockClass: typeof Block;
+  private _block: Block | null;
+  private _props: Props;
+
+  constructor(pathname: string, view: typeof Block, props: Props) {
+    this._pathname = pathname;
+    this._blockClass = view;
+    this._block = null;
+    this._props = props;
+  }
+
+  navigate(pathname: string) {
+    if (this.match(pathname)) {
+      this._pathname = pathname;
+      this.render();
+    }
+  }
+
+  leave() {
+    if (this._block) {
+      this._block.hide();
+    }
+  }
+
+  match(pathname: string) {
+    return pathname === this._pathname;
+  }
+
+  render() {
+    if (!this._block) {
+      this._block = new this._blockClass();
+      navigate(this._block, this._props._rootQuery);
+      return;
+    }
+
+    this._block.show();
+  }
+}
